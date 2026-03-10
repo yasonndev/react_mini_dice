ce20b5cc423692857e5ccc762259312b5b1966f8
Yan <yan.alyeyev@voe.com.ua>
2026-03-10 15:10:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DiceContainer from "./DiceContainer.jsx";
+
+function App() {
+  return (
+    <>
+        <DiceContainer />
+    </>
+  )
+}
+
+export default App
