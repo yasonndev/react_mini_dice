cd583bd653178fe276d535d4e7aefdf2a1795935
Yan <yan.alyeyev@voe.com.ua>
2026-03-11 09:50:33 +0200

    added deployment through ngrok tunnel

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,8 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+    server: {
+        host: true,      // Обязательно! Чтобы Vite слушал 0.0.0.0, а не только 127.0.0.1
+        port: 5173,      // Внутренний порт контейнера
+
+        // 1. Разрешаем домен ngrok
+        allowedHosts: [
+            'brett-lumpy-veronika.ngrok-free.dev',
+            'localhost',
+            '.ngrok-free.dev' // Разрешит любой поддомен ngrok (удобно!)
+        ],
+
+        // 2. Настройка Hot Module Replacement (HMR)
+        // Без этого правки в коде не будут отображаться в Telegram без перезагрузки
+        hmr: {
+            clientPort: 443, // Ngrok работает через 443 (https)
+            host: 'brett-lumpy-veronika.ngrok-free.dev',
+            protocol: 'wss'  // Используем защищенный WebSocket
+        },
+    },
+    plugins: [
       react(),
       tailwindcss(),
-  ],
+    ],
 })
